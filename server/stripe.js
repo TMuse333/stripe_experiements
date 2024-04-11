604e26d04d241485b425905d46c2feb369e55f35
Thomas Musial <th896493@dal.ca>
2024-04-11 15:29:40 -0300

    payment stable

--- server/stripe.js
+++ server/stripe.js
@@ -1,18 +1,21 @@
 // This is your test secret API key.
 import stripePackage from 'stripe';
 import express  from 'express';
+import Stripe from 'stripe'
 
-const stripe = stripePackage('sk_test_51P3fPHD53TrvLemWwuSGOAOuqJGjT7acM30ynx7yTPm94tvadkFtlOdTfPJqsFjuK7iLkTM4s1G5RNPhO3M1lsPl00K5e8ia6T');
+const stripe = new Stripe('sk_test_51P3fPHD53TrvLemWwuSGOAOuqJGjT7acM30ynx7yTPm94tvadkFtlOdTfPJqsFjuK7iLkTM4s1G5RNPhO3M1lsPl00K5e8ia6T');
 const app = express();
 app.use(express.static('public'));
 
 // Rest of your code...
 
+
+
 const router = express.Router()
 
 
 
-const YOUR_DOMAIN = 'http://localhost:5173';
+const YOUR_DOMAIN = 'http://localhost:3001';
 
 router.post('/create-checkout-session', async (req, res) => {
   const session = await stripe.checkout.sessions.create({
@@ -31,6 +34,23 @@ router.post('/create-checkout-session', async (req, res) => {
 
   res.send({url: session.url});
 });
+
+router.get('/get-price', async (req, res) => {
+  console.log('hello')
+  try {
+      const price = await stripe.prices.retrieve('price_1P3jCiD53TrvLemW2f6O5qNv'); 
+      // Replace 'pr_123456789' with your actual price ID
+   
+      res.json({ price });
+  } catch (error) {
+      console.error(error.message);
+      res.status(500).json({ error: 'Internal Server Error' });
+  }
+
+});
+
+
+
 
 export default router
 
